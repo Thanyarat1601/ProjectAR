b90f1bbf37dede307f8ff3b3bd97e4a45d88f460
Thanyarat Kambunrueng <108591357+Thanyarat1601@users.noreply.github.com>
2023-04-02 02:50:41 +0700

    hh

--- controllermain.js
+++ controllermain.js
@@ -1,4 +1,5 @@
 var app = angular.module('ViewWeb', []);
+
 app.directive('fileModel', ['$parse', function ($parse) { 
   return { 
      restrict: 'A', 
@@ -19,36 +20,30 @@ app.directive('fileModel', ['$parse', function ($parse) {
   }; 
 
 }]); 
+$scope.rtree = {};
 
+var fd = new FormData();
 
-        // document.addEventListener('ViewWebController',['$scope','$http', function($scope,$http,) {
-        // const input = document.getElementById('imageinput'); //1 
-        // const area = document.getElementById('previewarea');//2 
-        // input.onchange = (e) =>{ //3
-        //     let text = document.getElementById('text');
-        //     let bird1 = document.getElementById('bird1');
-        //     let bird2 = document.getElementById('bird2');
-        //     let btn = document.getElementById('btn');
-        //     let rocks = document.getElementById('rocks');
-        //     let forest = document.getElementById('forest');
-        //     let water = document.getElementById('water');
-        //     let header = document.getElementById('header');
-            
-        //     window.addEventListener('scroll', function() {
-        //         let value = window.scrollY;
-                
-        //         text.style.top = 50 + value * -.1 + '%';
-        //         bird2.style.top = value * -1.5 + 'px';
-        //         bird2.style.left = value * 2 + 'px';
-        //         bird1.style.top = value * -1.5 + 'px';
-        //         bird1.style.left = value * -5 + 'px';
-        //         // btn.style.marginTop = value * 1.5 + 'px';
-        //         rocks.style.top = value * -.12 + 'px';
-        //         forest.style.top = value * .40 + 'px';
-        //         header.style.top = value * .5 + 'px';
-        //     }
-        // }       
-        // }]);
+$scope.select = function() {
+    var tem = null;
+    if (angular.isUndefined($scope.search)) {
+        tem = 1;
+    } else {
+        tem = $scope.search;
+    }
+
+    $http({
+        method: 'post',
+        url: './DB/select.php',
+        data: {search: tem},
+    }).then(function mySuccess(response) {
+        $scope.rtree = response.data;
+    }, function myError(response) {
+        // Handle error response
+    });
+};
+
+var area = document.getElementById('previewarea');
 
 app.controller('ViewWebController',['$scope','$http', function($scope,$http,) {
   
@@ -63,10 +58,9 @@ app.controller('ViewWebController',['$scope','$http', function($scope,$http,) {
               img.onload = () => URL.revokeObjectURL(img.src);  //6 
         }; 
     }]);
- 
+
 document.addEventListener('ViewWebController',['$scope','$http', function($scope,$http,) {
     const input = document.getElementById('imageinput'); //1 
-    const area = document.getElementById('previewarea');//2 
     input.onchange = (e) =>{ //3
         let text = document.getElementById('text');
         let bird1 = document.getElementById('bird1');
@@ -92,57 +86,3 @@ document.addEventListener('ViewWebController',['$scope','$http', function($scope
         });
     }       
 }]);
-
-
-//  $scope.rtree = {};
- 
- 
-//  var fd = new FormData();
-
-//         $scope.select = function(){  
-//         var tem = null;
-//         if(angular.isUndefined($scope.search)) {
-//         tem = 1;
-//     }else{
-//         tem = $scope.search;
-//     }
-
-//         $http({method : 'post' ,
-//                url : './DB/select.php',
-//                data : {search:tem},
-          
-//     }).then( function  mySuccess(response){
-//                $scope.rtree = response.data;
-//             },  
-
-//             function myError(response){
-
-  
-//             });         
-//         };
-//     }]);
-
-
-
-    $scope.rtree = {};
-
-        var fd = new FormData();
-
-        $scope.select = function() {
-            var tem = null;
-            if (angular.isUndefined($scope.search)) {
-                tem = 1;
-            } else {
-                tem = $scope.search;
-            }
-
-            $http({
-                method: 'post',
-                url: './DB/select.php',
-                data: {search: tem},
-            }).then(function mySuccess(response) {
-                $scope.rtree = response.data;
-            }, function myError(response) {
-                // Handle error response
-            });
-        };
