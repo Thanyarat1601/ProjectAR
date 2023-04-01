b78111af4abc4d794edf8d3779c6bcd33d728e22
Thanyarat Kambunrueng <108591357+Thanyarat1601@users.noreply.github.com>
2023-04-02 03:14:20 +0700

    k

--- controllermain.js
+++ controllermain.js
@@ -20,48 +20,47 @@ app.directive('fileModel', ['$parse', function ($parse) {
   }; 
 
 }]); 
-$scope.rtree = {};
 
-var fd = new FormData();
+app.controller('ViewWebController', ['$scope', '$http', function($scope, $http) {
+    $scope.rtree = {};
+    var area = document.getElementById('previewarea');
 
-$scope.select = function() {
-    var tem = null;
-    if (angular.isUndefined($scope.search)) {
-        tem = 1;
-    } else {
-        tem = $scope.search;
-    }
+    $scope.select = function() {
+        var tem = null;
+        if (angular.isUndefined($scope.search)) {
+            tem = 1;
+        } else {
+            tem = $scope.search;
+        }
 
-    $http({
-        method: 'post',
-        url: 'selectmain.php',
-        data: {search: tem},
-    }).then(function mySuccess(response) {
-        $scope.rtree = response.data;
-    }, function myError(response) {
-        // Handle error response
-    });
-};
+        $http({
+            method: 'post',
+            url: './DB/select.php',
+            data: {search: tem},
+        }).then(function mySuccess(response) {
+            $scope.rtree = response.data;
+        }, function myError(response) {
+            // Handle error response
+        });
+    };
 
-var area = document.getElementById('previewarea');
-
-app.controller('ViewWebController',['$scope','$http', function($scope,$http,) {
-  
-        while (area.hasChildNodes()) { //3 
-              area.removeChild(area.firstChild); 
-        }; 
-        for (var i = 0; i < e.target.files.length; i++) { //4 
-              let img = new Image(); //4 
-              img.width = 100;       //4 
-              img.src = URL.createObjectURL(e.target.files[i]);  //4 
-              area.appendChild(img);  //5 
-              img.onload = () => URL.revokeObjectURL(img.src);  //6 
-        }; 
-    }]);
-
-document.addEventListener('ViewWebController',['$scope','$http', function($scope,$http,) {
+    while (area.hasChildNodes()) { //3 
+        area.removeChild(area.firstChild); 
+    }; 
     const input = document.getElementById('imageinput'); //1 
-    input.onchange = (e) =>{ //3
+    input.onchange = (e) => { //3
+        for (var i = 0; i < e.target.files.length; i++) { //4 
+            let img = new Image(); //4 
+            img.width = 100;       //4 
+            img.src = URL.createObjectURL(e.target.files[i]);  //4 
+            area.appendChild(img);  //5 
+            img.onload = () => URL.revokeObjectURL(img.src);  //6 
+        }; 
+    };
+
+    window.addEventListener('scroll', function() {
+        let value = window.scrollY;
+
         let text = document.getElementById('text');
         let bird1 = document.getElementById('bird1');
         let bird2 = document.getElementById('bird2');
@@ -71,18 +70,14 @@ document.addEventListener('ViewWebController',['$scope','$http', function($scope
         let water = document.getElementById('water');
         let header = document.getElementById('header');
 
-        window.addEventListener('scroll', function() {
-            let value = window.scrollY;
-
-            text.style.top = 50 + value * -.1 + '%';
-            bird2.style.top = value * -1.5 + 'px';
-            bird2.style.left = value * 2 + 'px';
-            bird1.style.top = value * -1.5 + 'px';
-            bird1.style.left = value * -5 + 'px';
-            // btn.style.marginTop = value * 1.5 + 'px';
-            rocks.style.top = value * -.12 + 'px';
-            forest.style.top = value * .40 + 'px';
-            header.style.top = value * .5 + 'px';
-        });
-    }       
+        text.style.top = 50 + value * -.1 + '%';
+        bird2.style.top = value * -1.5 + 'px';
+        bird2.style.left = value * 2 + 'px';
+        bird1.style.top = value * -1.5 + 'px';
+        bird1.style.left = value * -5 + 'px';
+        btn.style.marginTop = value * 1.5 + 'px';
+        rocks.style.top = value * -.12 + 'px';
+        forest.style.top = value * .40 + 'px';
+        header.style.top = value * .5 + 'px';
+    });
 }]);
