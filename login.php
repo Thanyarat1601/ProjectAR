da4e7a8df0064fc6c194bddb88d90de3000a131e
Thanyarat Kambunrueng <108591357+Thanyarat1601@users.noreply.github.com>
2023-04-08 06:28:55 +0700

    login

--- login.php
+++ login.php
@@ -1,68 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Admin</title>
-	<link rel="stylesheet" type="text/css" href="style2.css">
-	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-	<?php if (isset($error_message)) { ?>
-		<div><?php echo $error_message; ?></div>
-	  <?php } ?>
-	<img class="wave" src="img/wave.png">
-	<div class="container">
-		<div class="img">
-			<img src="img/bg.svg">
-		</div>
-		<div class="login-content">
-			<form action="index.html">
-				<img src="img/avatar.svg">
-				<h2 class="title">Welcome</h2>
-           		<div class="input-div one">
-           		   <div class="i">
-           		   		<i class="fas fa-user"></i>
-           		   </div>
-           		   <div class="div">
-           		   		<h5>Username</h5>
-           		   		<input type="text" class="input">
-           		   </div>
-           		</div>
-           		<div class="input-div pass">
-           		   <div class="i"> 
-           		    	<i class="fas fa-lock"></i>
-           		   </div>
-           		   <div class="div">
-           		    	<h5>Password</h5>
-           		    	<input type="password" class="input">
-            	   </div>
-            	</div>
-            	<a href="#">Forgot Password?</a>
-            	<input type="submit" class="btn" value="Login">
-            </form>
-        </div>
-    </div>
-    <script type="text/javascript" src="main.js"></script>
-</body>
-</html>
-<?php
-session_start();
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-  header('Location: login.php');
-  exit;
-}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Admin Dashboard</title>
-</head>
-<body>
-  <h1>Welcome to the Admin Dashboard!</h1>
-  <!-- Admin Dashboard content here -->
-</body>
-</html>
 <?php
 session_start();
 
@@ -74,6 +9,11 @@ $dbname = "plants";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+if ($result->num_rows > 0) {
+  $_SESSION['logged_in'] = true;
+  header('Location: indexDB.html');
+  exit;
+}
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -103,3 +43,22 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
   $conn->close();
 }
 ?>
+
+
+<?php
+session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+  header('Location: login.php');
+  exit;
+}
+?> 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Admin Dashboard</title>
+</head>
+<body>
+  <h1>Welcome to the Admin Dashboard!</h1>
+  <!-- Admin Dashboard content here -->
+</body>
+</html> 
