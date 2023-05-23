da05b04a99a76470f4111fe26795026ea33520e2
Ketsinee <always04937@gmail.com>
2023-05-24 04:00:04 +0700

    heal
    heal

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const inputs = document.querySelectorAll(".input");
 
+// Check if there is an error message
+var errorMessage = "<?php echo isset($_SESSION['error_message']) ? $_SESSION['error_message'] : ''; ?>";
+var errorClass = "<?php echo isset($_SESSION['error_class']) ? $_SESSION['error_class'] : ''; ?>";
+
+if (errorMessage !== '') {
+  var usernameInput = document.querySelector('input[name="username"]');
+  var passwordInput = document.querySelector('input[name="password"]');
+
+  usernameInput.classList.add(errorClass);
+  passwordInput.classList.add(errorClass);
+}
 
 function addcl(){
 	let parent = this.parentNode.parentNode;
@@ -17,4 +28,5 @@ function remcl(){
 inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
+	
 });
