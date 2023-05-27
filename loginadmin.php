8f49666a90ad8385a8c2330dbbf3a91a6f1a033e
Thanyarat Kambunrueng <108591357+Thanyarat1601@users.noreply.github.com>
2023-05-28 03:00:51 +0700

    Aom

--- loginadmin.php
+++ loginadmin.php
@@ -18,34 +18,28 @@
 			<form action="login.php" method="POST">
 				<img src="img/002.svg">
 				<h2 class="title">Welcome</h2>
-				<div class="input-div one <?php echo isset($_SESSION['error_message']) ? 'invalid' : ''; ?>">
+				<div class="input-div one ">
 					<div class="i">
 					  <i class="fas fa-user"></i>
 					</div>
 					<div class="div">
 					  <h5>Username</h5>
-					  <input type="text" class="input <?php echo isset($_SESSION['error_message']) ? 'invalid' : ''; ?>" name="username" required>
+					  <input type="text" class="input " name="username" required>
 					</div>
 				  </div>
-				  <div class="input-div pass <?php echo isset($_SESSION['error_message']) ? 'invalid' : ''; ?>">
+				  <div class="input-div pass ">
 					<div class="i"> 
 					  <i class="fas fa-lock"></i>
 					</div>
 					<div class="div">
 					  <h5>Password</h5>
-					  <input type="password" class="input <?php echo isset($_SESSION['error_message']) ? 'invalid' : ''; ?>" name="password" required>
+					  <input type="password" class="input " name="password" required>
 					</div>
 				  </div>
-						  
+				  	  
 				<a href="#">Forgot Password?</a>
 				<input type="submit" class="btn" value="Login">
 			</form>
-			<?php
-				if (isset($_SESSION['error_message'])) {
-					echo '<div class="error-message">' . $_SESSION['error_message'] . '</div>';
-					unset($_SESSION['error_message']);
-				}
-			?>
 		</div>
 	</div>
 	<script type="text/javascript" src="main.js"></script>
--- main.js
+++ main.js
@@ -5,20 +5,6 @@ inputs.forEach(input => {
     input.addEventListener("blur", remcl);
 });
 
-// Check if there is an error message
-var errorMessage = "<?php echo isset($_SESSION['error_message']) ? $_SESSION['error_message'] : ''; ?>";
-var errorClass = "<?php echo isset($_SESSION['error_message']) ? 'invalid' : ''; ?>";
-
-if (errorMessage !== '') {
-    var usernameInput = document.querySelector('input[name="username"]');
-    var passwordInput = document.querySelector('input[name="password"]');
-
-    usernameInput.classList.add(errorClass);
-    passwordInput.classList.add(errorClass);
-    usernameInput.parentNode.parentNode.classList.add("error");
-    passwordInput.parentNode.parentNode.classList.add("error");
-}
-
 function addcl() {
     let parent = this.parentNode.parentNode;
     parent.classList.add("focus");
@@ -32,4 +18,15 @@ function remcl() {
     if (this.value == "") {
         parent.classList.remove("focus");
     }
+}
+
+// Check if there is an error message
+var errorMessage = "<?php echo isset($_SESSION['error_message']) ? $_SESSION['error_message'] : ''; ?>";
+
+if (errorMessage !== '') {
+    var usernameInput = document.querySelector('input[name="username"]');
+    var passwordInput = document.querySelector('input[name="password"]');
+
+    usernameInput.parentNode.parentNode.classList.add("invalid");
+    passwordInput.parentNode.parentNode.classList.add("invalid");
 }
