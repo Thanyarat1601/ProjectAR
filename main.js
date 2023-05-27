c7ed3fa26bc331a0ca37b4eee17619b4fbc17278
Thanyarat Kambunrueng <108591357+Thanyarat1601@users.noreply.github.com>
2023-05-28 03:04:32 +0700

    Aom

--- main.js
+++ main.js
@@ -5,6 +5,23 @@ inputs.forEach(input => {
     input.addEventListener("blur", remcl);
 });
 
+// Check if there is an error message
+var errorMessage = "<?php echo isset($_SESSION['error_message']) ? $_SESSION['error_message'] : ''; ?>";
+
+if (errorMessage !== '') {
+    var usernameInput = document.querySelector('input[name="username"]');
+    var passwordInput = document.querySelector('input[name="password"]');
+    var loginForm = document.querySelector('form[action="login.php"]');
+
+    loginForm.addEventListener('submit', function(event) {
+        if (usernameInput.value === '' || passwordInput.value === '') {
+            event.preventDefault(); // ไม่ส่งฟอร์มเมื่อข้อมูลไม่ครบ
+            usernameInput.parentNode.parentNode.classList.add('invalid');
+            passwordInput.parentNode.parentNode.classList.add('invalid');
+        }
+    });
+}
+
 function addcl() {
     let parent = this.parentNode.parentNode;
     parent.classList.add("focus");
@@ -18,15 +35,4 @@ function remcl() {
     if (this.value == "") {
         parent.classList.remove("focus");
     }
-}
-
-// Check if there is an error message
-var errorMessage = "<?php echo isset($_SESSION['error_message']) ? $_SESSION['error_message'] : ''; ?>";
-
-if (errorMessage !== '') {
-    var usernameInput = document.querySelector('input[name="username"]');
-    var passwordInput = document.querySelector('input[name="password"]');
-
-    usernameInput.parentNode.parentNode.classList.add("invalid");
-    passwordInput.parentNode.parentNode.classList.add("invalid");
 }
