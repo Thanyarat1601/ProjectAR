8bdbdd934de27e03a724e0230b6b91e901fc4ee4
Ketsinee <always04937@gmail.com>
2023-05-31 00:23:09 +0700

    heal

--- loginadmin.php
+++ loginadmin.php
@@ -16,23 +16,23 @@
 			<img src="img/Crop_04.svg">
 		</div>
 		<div class="login-content">
-			<form action="login.php" method="POST" onsubmit="return validated();">
+			<form action="login.php" method="POST">
 				<img src="img/002.svg">
 				<h2 class="title">Welcome</h2>
-				<div class="input-div one <?php echo !isset($_SESSION['error_class']) && $_SESSION['error_class'] === 'invalid' ? 'invalid' : ''; ?>">
+				<div class="input-div one">
 					<div class="i">
 					  <i class="fas fa-user"></i>
 					</div>
-					<div class="div <?php echo !isset($_SESSION['error_class']) && $_SESSION['error_class'] === 'invalid' ? 'invalid' : ''; ?>">
+					<div class="div">
 					  <h5>Username</h5>
 					  <input type="text" class="input" name="username" required>
 					</div>
 				  </div>
-				  <div class="input-div pass <?php echo !isset($_SESSION['error_class']) && $_SESSION['error_class'] === 'invalid' ? 'invalid' : ''; ?>">
+				  <div class="input-div pass">
 					<div class="i"> 
 					  <i class="fas fa-lock"></i>
 					</div>
-					<div class="div <?php echo !isset($_SESSION['error_class']) && $_SESSION['error_class'] === 'invalid' ? 'invalid' : ''; ?>">
+					<div class="div">
 					  <h5>Password</h5>
 					  <input type="password" class="input" name="password" required>
 					</div>
@@ -44,32 +44,6 @@
 		</div>
 	</div>
 	<script type="text/javascript" src="main.js"></script>
-	<script>
-	var errorMessage = "<?php echo isset($_SESSION['error_message']) ? $_SESSION['error_message'] : ''; ?>";
-
-	if (errorMessage !== '') {
-		var usernameInput = document.querySelector('input[name="username"]');
-		var passwordInput = document.querySelector('input[name="password"]');
-		var loginForm = document.querySelector('form[action="login.php"]');
-
-		if (usernameInput.classList.contains('invalid')) {
-			usernameInput.parentNode.classList.add('invalid');
-		}
-
-		if (passwordInput.classList.contains('invalid')) {
-			passwordInput.parentNode.classList.add('invalid');
-		}
-
-		loginForm.addEventListener('submit', function(event) {
-			if (usernameInput.value === '' || passwordInput.value === '') {
-				event.preventDefault();
-				usernameInput.parentNode.classList.add('invalid');
-				passwordInput.parentNode.classList.add('invalid');
-			}
-		});
-	}
-	
-</script>
 
 </body>
 
