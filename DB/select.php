96ca7d9872ec0affb970cf34ba855be2a0e77c81
Thanyarat Kambunrueng <108591357+Thanyarat1601@users.noreply.github.com>
2023-09-23 01:35:26 +0700

    Aom

--- DB/select.php
+++ DB/select.php
@@ -1,22 +1,45 @@
-
 <?php
-include "database_connection.php";
-$x = file_get_contents("php://input");
-$d = json_decode($x);
-if(($d->search==1)){
-    $a = 1;
-}else{
-    $a = "ID Like'{$d->search}' ";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "plants";
+
+// Establish database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT * FROM `tree` WHERE {$a}";
+
+// Get search query parameter
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+
+// Build SQL query based on search query parameter
+if ($search == '') {
+  $sql = "SELECT * FROM `new_table`";
+} else {
+  $sql = "SELECT * FROM new_table WHERE ID LIKE '%{$search}%' OR thainame LIKE '%{$search}%' OR engname LIKE '%{$search}%' OR properties LIKE '%{$search}%' OR treetyyy LIKE '%{$search}%'";
+}
+
+// Execute SQL query
 $result = $conn->query($sql);
 
-
+// Build JSON response
 $rows = array();
-while($r = $result->fetch_assoc()){
-    $rows[] = $r;
+while ($row = $result->fetch_assoc()) {
+    // แปลงข้อมูล BLOB เป็น base64
+    if (!empty($row['qrcodeofar'])) {
+        $row['qrcodeofar'] = base64_encode($row['qrcodeofar']);
+    }
+    $rows[] = $row;
 }
-  print json_encode($rows);
-  $conn->close();
+$json_response = json_encode($rows);
 
+// Close database connection
+$conn->close();
+
+// Return JSON response
+header('Content-Type: application/json');
+echo $json_response;
 ?>
